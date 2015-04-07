04501f368b72b5786113fcd37905d4b87034051c
Clancy Emanuel <clancy.emanuel@uconn.edu>
2015-04-06 22:45:51 -0400

    best version yet. target detection is good and depth is tuned for looking for targets.

--- laser_ws/src/laser_package/src/drivers/detect_driver.cpp
+++ laser_ws/src/laser_package/src/drivers/detect_driver.cpp
@@ -8,36 +8,40 @@ std::vector<float> detectTargets(std::vector<float> ranges)
 	float mean_range = 0.0;
 	int mean_index;
 	new_series = 1;
-	for (int i = 0;i<682;i++) //iterate through the data
+	for (int i = 100;i<570;i++) //iterate through the data
 	{
-		if(new_series) //if we're moving on to a new potential target, reset variables
+		if(ranges[i]<4.8)
 		{
-			place_keeper = i;
-			num_points_min = floor(MIN_TARGET_WIDTH/(ranges[place_keeper]*THETA_DELTA));
-			num_points_max = floor(MAX_TARGET_WIDTH/(ranges[place_keeper]*THETA_DELTA));
-			new_series = 0;	
-			num_points = 1;
-		}
-		if(abs(ranges[i+1]-ranges[place_keeper])<MAX_TARGET_DEPTH) //if the target is reasonably deep
-		{
-			num_points++; //then increment num_points
-		}
-		else //if we come across a point that is too far from the first point
-		{
-			if(num_points>num_points_min&&num_points<num_points_max) //check if num_points makes sense
+			if(new_series) //if we're moving on to a new potential target, reset variables
 			{
-				for(int j = place_keeper;j<place_keeper+num_points;j++)//if it does, enter the target in a new array
-				{
-					filtered_values[j] = ranges[j];
-					mean_range += ranges[j];
-				}
-				mean_range /= num_points;
-				mean_index = place_keeper + floor(num_points/2);
-				point_targets[mean_index] = mean_range;
+				place_keeper = i;
+				num_points_min = floor(MIN_TARGET_WIDTH/(ranges[place_keeper]*THETA_DELTA));
+				num_points_max = floor(MAX_TARGET_WIDTH/(ranges[place_keeper]*THETA_DELTA));
+				new_series = 0;	
+				num_points = 1;
 			}
-			num_points = 0;
-			new_series = 1;//start looking for more targets
+			if(abs(ranges[i+1]-ranges[place_keeper])<MAX_TARGET_DEPTH) //if the target is reasonably deep
+			{
+				num_points++; //then increment num_points
+			}
+			else //if we come across a point that is too far from the first point
+			{
+				if(num_points>num_points_min&&num_points<num_points_max) //check if num_points makes sense
+				{
+					/*for(int j = place_keeper;j<place_keeper+num_points;j++)//if it does, enter the target in a new array
+					{
+						filtered_values[j] = ranges[j];
+						mean_range += ranges[j];
+					}*/
+					mean_range /= num_points;
+					mean_index = place_keeper + floor(num_points/2);
+					point_targets[mean_index] = ranges[mean_index];
+					mean_range = 0;
+				}
+				num_points = 0;
+				new_series = 1;//start looking for more targets
+			}
 		}
 	}
-	return point_targets; //change to filtered_ranges to show extended targets
+	return point_targets; //change to filtered_ranges to show extended targets, point_targets for point targets
 }
