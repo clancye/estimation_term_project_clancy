4f726ca195179a7487dedef537a39b43fd82f965
Clancy Emanuel <clancy.emanuel@uconn.edu>
2015-04-19 02:46:50 -0400

    hi

--- laser_ws/src/laser_package/src/drivers/detect_driver.cpp
+++ laser_ws/src/laser_package/src/drivers/detect_driver.cpp
@@ -42,7 +42,7 @@ std::vector<float> Detector::detectTargets(std::vector<float> ranges)
 								break;
 							case WAITING_FOR_MOVING_TARGET:
 								zone = getZone(mean_range,mean_index);
-								if(zone != -1)updateVelocity(zone,mean_range, mean_index,scan_time);
+							//	if(zone != -1)updateVelocity(zone,mean_range, mean_index,scan_time);
 								point_targets = initial_targets;
 								break;
 							case TRACKING:
