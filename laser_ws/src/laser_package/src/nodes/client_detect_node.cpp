386db9d4e95ed4bba9714fd59d13479497cf173c
Clancy Emanuel <clancy.emanuel@uconn.edu>
2015-04-18 22:35:00 -0400

    asdfasdf

--- laser_ws/src/laser_package/src/nodes/client_detect_node.cpp
+++ laser_ws/src/laser_package/src/nodes/client_detect_node.cpp
@@ -8,14 +8,15 @@ class SubscribeAndPublish
 	public:
 		SubscribeAndPublish()
 		{
-			ROS_INFO("Constructing SAP");
+			ROS_INFO("Constructing SAP...");
+			ros::Duration(10).sleep(); // sleep for 10 seconds
 			laser_sub = n.subscribe<sensor_msgs::LaserScan>("/client_scan",1000,&SubscribeAndPublish::laserCallBack,this);
 			target_pub = n.advertise<sensor_msgs::LaserScan>("/client_targets",1000); //publish targets to new topic
 			std::vector<float> filtered_ranges (682);
 			std::vector<float> initial_targets (682);
 			scan_counter = 0;
 			state = INITIALIZING;
-			//ros::Duration(10).sleep(); // sleep for 10 seconds
+			ROS_INFO("Go!");
 		}
 		
 
@@ -23,12 +24,11 @@ class SubscribeAndPublish
 		{ 
 			targets_msg = *msg;
 			target_detector.setState(state);
-			filtered_ranges = target_detector.detectTargets(msg->ranges);
-			targets_msg.ranges = filtered_ranges;
-			target_pub.publish(targets_msg);//NEW CODE ADDED CONITIONAL
-			if(scan_counter<=10)scan_counter++;//NEW CODE
+			targets_msg.ranges = target_detector.detectTargets(msg->ranges);
+			target_pub.publish(targets_msg);
+			if(scan_counter<=10)scan_counter++;
 			else{state = WAITING_FOR_MOVING_TARGET;};
-		}//dfd
+		}
 	private:
 	   
 		ros::NodeHandle n; 
@@ -44,7 +44,7 @@ class SubscribeAndPublish
 int main(int argc, char **argv)
 {
 	//Initiate ROS
-	ros::init(argc, argv, "detect_node");
+	ros::init(argc, argv, "client_detect_node");
 
 	//Create an object of class SubscribeAndPublish that will take care of everything
 	SubscribeAndPublish SAPDetect;
