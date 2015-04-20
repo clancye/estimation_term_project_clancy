dda72bb9b06f1d0dcc46506bb27237f965ce8c1f
Clancy Emanuel <clancy.emanuel@uconn.edu>
2015-04-20 15:34:29 -0400

    again with the wall

--- laser_ws/src/laser_package/src/drivers/detect_driver.cpp
+++ laser_ws/src/laser_package/src/drivers/detect_driver.cpp
@@ -34,7 +34,7 @@ std::vector<float> Detector::detectTargets(std::vector<float> ranges)
 						{
 							int zone;
 							case INITIALIZING:
-								if(abs(ranges[place_keeper-8]-ranges[place_keeper+1])>WALL_DISTANCE_THRESHOLD&&abs(ranges[last_index+8]-ranges[last_index-1])>WALL_DISTANCE_THRESHOLD);
+								if(abs(ranges[place_keeper-4]-ranges[place_keeper+1])>WALL_DISTANCE_THRESHOLD&&abs(ranges[last_index+4]-ranges[last_index-1])>WALL_DISTANCE_THRESHOLD);
 								{
 									if(getZone(mean_range,mean_index)==-1) 
 									{
