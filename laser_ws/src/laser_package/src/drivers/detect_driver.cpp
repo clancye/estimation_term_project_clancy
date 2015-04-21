92ced7c3b7d2a6e2de8a6b3ae5ea6bb6a7f126d3
Clancy Emanuel <clancy.emanuel@uconn.edu>
2015-04-21 01:52:47 -0400

    changed range requirements

--- laser_ws/src/laser_package/src/drivers/detect_driver.cpp
+++ laser_ws/src/laser_package/src/drivers/detect_driver.cpp
@@ -22,7 +22,7 @@ std::vector<float> Detector::detectTargets(std::vector<float> ranges)
 				new_series = 0;	
 				num_points = 1;
 			}
-			if(abs(ranges[i+1]-ranges[place_keeper])<MAX_TARGET_DEPTH) //if the target is reasonably deep
+			if(state!= TRACKING&&(abs(ranges[i+1]-ranges[place_keeper])<MAX_TARGET_DEPTH)) //if the target is reasonably deep
 			{
 				num_points++; //then increment num_points
 			}
