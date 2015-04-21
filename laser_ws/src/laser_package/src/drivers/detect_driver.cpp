0f4debe362c6791f3fb53f2e6f24b28870bff681
Clancy Emanuel <clancy.emanuel@uconn.edu>
2015-04-20 21:49:53 -0400

    realized i was using the wrong variables in getZone

--- laser_ws/src/laser_package/src/drivers/detect_driver.cpp
+++ laser_ws/src/laser_package/src/drivers/detect_driver.cpp
@@ -70,8 +70,8 @@ std::vector<float> Detector::detectTargets(std::vector<float> ranges)
 								}
 								//else{ROS_INFO("NOPE. ZONE = %d\n and zoneBeingTracked = %d",zone,getZoneBeingTracked());}
 								tracked_targets[mean_index] = mean_range;
-								tracked_targets[start_step-2] = mean_range;
-								tracked_targets[end_step+2] = mean_range;
+								tracked_targets[start_index-2] = mean_range;
+								tracked_targets[end_index+2] = mean_range;
 								point_targets = tracked_targets;
 								break;
 						}
