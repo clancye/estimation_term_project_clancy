125e9a41d03624f1e3a93e17bd9b195759d9a515
Clancy Emanuel <clancy.emanuel@uconn.edu>
2015-04-21 00:27:35 -0400

    again

--- laser_ws/src/laser_package/src/nodes/client_detect_node.cpp
+++ laser_ws/src/laser_package/src/nodes/client_detect_node.cpp
@@ -28,8 +28,8 @@ class SubscribeAndPublish
 			target_detector.setState(state);
 			targets_msg.ranges = target_detector.detectTargets(msg->ranges);
 			
-			if(scan_counter<=10)scan_counter++;//time to gather some possible targets
-			if(scan_counter>10&&target_detector.getNumberOfZones() >0 )state = WAITING_FOR_MOVING_TARGET;//keep initializing if no targets
+			//if(scan_counter<=10)scan_counter++;//time to gather some possible targets
+			//if(scan_counter>10&&target_detector.getNumberOfZones() >0 )state = WAITING_FOR_MOVING_TARGET;//keep initializing if no targets
 			else if (scan_counter > 10 && target_detector.getNumberOfZones() == 0){scan_counter = 0;}//once we have some targets, wait until they move
 			if (target_detector.getZoneBeingTracked() != -1) state = TRACKING;
 			if (state == TRACKING)
